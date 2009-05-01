c965f4bbaef939cc5526e76f2fb7abddd889d14a
activesys <activesys.wb@gmail.com>
2009-05-01 05:05:48 +0000

    Simplify the comments of queue_t and priority_queue_t interface.

--- include/cstl_queue.h
+++ include/cstl_queue.h
@@ -50,384 +50,75 @@ extern "C" {
 
 /** exported function prototype section **/
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_init
- *        Parameters: in) pt_queue: queue_t*
- *           Returns: void
- *       Description: initialize the queue.
- *
- * ----------------------------------------------------------------------------
+ * Initialization and destroy operation functions.
  */
 extern void queue_init(queue_t* pt_queue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_destroy
- *        Parameters: in) pt_queue: queue_t*
- *           Returns: void
- *       Description: destroy the queue.
- *
- * ----------------------------------------------------------------------------
- */
 extern void queue_destroy(queue_t* pt_queue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_init_copy
- *        Parameters: in) pt_queuedest: queue_t*
- *                          the dest queue.
- *                    in) cpt_queuesrc: const queue_t*
- *                          the source queue.
- *           Returns: void
- *       Description: the copy constructor.
- *
- * ----------------------------------------------------------------------------
- */
 extern void queue_init_copy(queue_t* pt_queuedest, const queue_t* cpt_queuesrc);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_assign
- *        Parameters: in) pt_queuedest: queue_t*
- *                          the dest queue.
- *                    in) cpt_queuesrc: const queue_t*
- *                          the source queue.
- *           Returns: void
- *       Description: copy the queue.
- *
- * ----------------------------------------------------------------------------
+ * Assign operator functions.
  */
 extern void queue_assign(queue_t* pt_queuedest, const queue_t* cpt_queuesrc);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_empty
- *        Parameters: in) cpt_queue: const queue_t*
- *                          the dest queue.
- *           Returns: bool_t
- *       Description: if the queue is empty.
- *
- * ----------------------------------------------------------------------------
+ * queue_t size operation functions.
  */
 extern bool_t queue_empty(const queue_t* cpt_queue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_size
- *        Parameters: in) cpt_queue: const queue_t*
- *                          the dest queue.
- *           Returns: size_t
- *       Description: get the queue size.
- *
- * ----------------------------------------------------------------------------
- */
 extern size_t queue_size(const queue_t* cpt_queue);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_front
- *        Parameters: in) cpt_queue: const queue_t*
- *                          the queue.
- *           Returns: void*
- *       Description: return the first element.
- *
- * ----------------------------------------------------------------------------
+ * Element access operation functions.
  */
 extern void* queue_front(const queue_t* cpt_queue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_back
- *        Parameters: in) cpt_queue: const queue_t*
- *                          the queue.
- *           Returns: void*
- *       Description: return the last element.
- *
- * ----------------------------------------------------------------------------
- */
 extern void* queue_back(const queue_t* cpt_queue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_pop
- *        Parameters: in) pt_queue: queue_t*
- *                          the queue.
- *           Returns: void
- *       Description: remove the top element and does not return it.
- *
- * ----------------------------------------------------------------------------
- */
 extern void queue_pop(queue_t* pt_queue);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_equal
- *        Parameters: in) cpt_queuefirst: const queue_t*
- *                          the first queue.
- *                    in) cpt_queuesecond: const queue_t*
- *                          the second queue.
- *           Returns: bool_t
- *       Description: return whether first queue equal to second queue.
- *
- * ----------------------------------------------------------------------------
+ * Relationship operator functions.
  */
-extern bool_t queue_equal(
-    const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_not_equal
- *        Parameters: in) cpt_queuefirst: const queue_t*
- *                          the first queue.
- *                    in) cpt_queuesecond: const queue_t*
- *                          the second queue.
- *           Returns: bool_t
- *       Description: return whether first queue not equal to second queue.
- *
- * ----------------------------------------------------------------------------
- */
+extern bool_t queue_equal(const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
 extern bool_t queue_not_equal(
     const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_less
- *        Parameters: in) cpt_queuefirst: const queue_t*
- *                          the first queue.
- *                    in) cpt_queuesecond: const queue_t*
- *                          the second queue.
- *           Returns: bool_t
- *       Description: return whether first queue less then second queue.
- *
- * ----------------------------------------------------------------------------
- */
-extern bool_t queue_less(
-    const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_great
- *        Parameters: in) cpt_queuefirst: const queue_t*
- *                          the first queue.
- *                    in) cpt_queuesecond: const queue_t*
- *                          the second queue.
- *           Returns: bool_t
- *       Description: return whether first queue greater then second queue.
- *
- * ----------------------------------------------------------------------------
- */
-extern bool_t queue_great(
-    const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_less_equal
- *        Parameters: in) cpt_queuefirst: const queue_t*
- *                          the first queue.
- *                    in) cpt_queuesecond: const queue_t*
- *                          the second queue.
- *           Returns: bool_t
- *       Description: return whether first queue less then or equal to the 
- *                    second queue.
- *
- * ----------------------------------------------------------------------------
- */
+extern bool_t queue_less(const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
+extern bool_t queue_great(const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
 extern bool_t queue_less_equal(
     const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: queue_great_equal
- *        Parameters: in) cpt_queuefirst: const queue_t*
- *                          the first queue.
- *                    in) cpt_queuesecond: const queue_t*
- *                          the second queue.
- *           Returns: bool_t
- *       Description: return whether first queue greater then or equal to the
- *                    second queue.
- *
- * ----------------------------------------------------------------------------
- */
 extern bool_t queue_great_equal(
     const queue_t* cpt_queuefirst, const queue_t* cpt_queuesecond);
 
 /* priority queue */
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_init
- *        Parameters: in) pt_pqueue: priority_queue_t*
- *           Returns: void
- *       Description: initialize the priority queue.
- *
- * ----------------------------------------------------------------------------
+ * Initialization and destroy operation functions.
  */
 extern void priority_queue_init(priority_queue_t* pt_pqueue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_init_op
- *        Parameters: in) pt_pqueue: priority_queue_t*
- *                    in) t_binary_op: binary_function_t
- *           Returns: void
- *       Description: initialize the priority queue.
- *
- * ----------------------------------------------------------------------------
- */
 extern void priority_queue_init_op(
     priority_queue_t* pt_pqueue, binary_function_t t_binary_op);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_destroy
- *        Parameters: in) pt_pqueue: priority_queue_t*
- *           Returns: void
- *       Description: destroy the priority queue.
- *
- * ----------------------------------------------------------------------------
- */
 extern void priority_queue_destroy(priority_queue_t* pt_pqueue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_init_copy
- *        Parameters: in) pt_pqueuedest: priority_queue_t*
- *                          the dest priority queue.
- *                    in) cpt_pqueuesrc: const priority_queue_t*
- *                          the source priority queue.
- *           Returns: void
- *       Description: the copy constructor.
- *
- * ----------------------------------------------------------------------------
- */
 extern void priority_queue_init_copy(
     priority_queue_t* pt_pqueuedest, const priority_queue_t* cpt_pqueuesrc);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_init_copy_range
- *        Parameters: in) pt_pqueuedest: priority_queue_t*
- *                          the dest priority queue.
- *                    in) t_first: random_access_iterator_t
- *                          the first iterator.
- *                    in) t_last: random_access_iterator_t
- *                          the first iterator.
- *           Returns: void
- *       Description: the copy constructor.
- *
- * ----------------------------------------------------------------------------
- */
 extern void priority_queue_init_copy_range(
     priority_queue_t* pt_pqueuedest,
-    random_access_iterator_t t_first,
-    random_access_iterator_t t_last);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_init_copy_range_op
- *        Parameters: in) pt_pqueuedest: priority_queue_t*
- *                          the dest priority queue.
- *                    in) t_first: random_access_iterator_t
- *                          the first iterator.
- *                    in) t_last: random_access_iterator_t
- *                          the first iterator.
- *                    in) t_binary_op: binary_function_t
- *           Returns: void
- *       Description: the copy constructor.
- *
- * ----------------------------------------------------------------------------
- */
+    random_access_iterator_t t_first, random_access_iterator_t t_last);
 extern void priority_queue_init_copy_range_op(
-    priority_queue_t* pt_pqueuedest,
-    random_access_iterator_t t_first,
-    random_access_iterator_t t_last,
-    binary_function_t t_binary_op);
+    priority_queue_t* pt_pqueuedest, random_access_iterator_t t_first,
+    random_access_iterator_t t_last, binary_function_t t_binary_op);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_assign
- *        Parameters: in) pt_pqueuedest: priority_queue_t*
- *                          the dest priority queue.
- *                    in) cpt_pqueuesrc: const priority queue_t*
- *                          the source priority queue.
- *           Returns: void
- *       Description: copy the priority queue.
- *
- * ----------------------------------------------------------------------------
+ * Assign operator functions.
  */
 extern void priority_queue_assign(
     priority_queue_t* pt_pqueuedest, const priority_queue_t* cpt_pqueuesrc);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_empty
- *        Parameters: in) cpt_pqueue: const priority_queue_t*
- *                          the dest priority queue.
- *           Returns: bool_t
- *       Description: if the priority queue is empty.
- *
- * ----------------------------------------------------------------------------
+ * priority_queue_t size operation functions.
  */
 extern bool_t priority_queue_empty(const priority_queue_t* cpt_pqueue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_size
- *        Parameters: in) cpt_pqueue: const priority_queue_t*
- *                          the dest priority queue.
- *           Returns: size_t
- *       Description: get the priority queue size.
- *
- * ----------------------------------------------------------------------------
- */
 extern size_t priority_queue_size(const priority_queue_t* cpt_pqueue);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_top
- *        Parameters: in) cpt_pqueue: const priority_queue_t*
- *                          the priority queue.
- *           Returns: void*
- *       Description: return the top element.
- *
- * ----------------------------------------------------------------------------
- */
 extern void* priority_queue_top(const priority_queue_t* cpt_pqueue);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: priority_queue_pop
- *        Parameters: in) pt_pqueue: priority_queue_t*
- *                          the priority queue.
- *           Returns: void
- *       Description: remove the top element and does not return it.
- *
- * ----------------------------------------------------------------------------
+ * Element access functions.
  */
 extern void priority_queue_pop(priority_queue_t* pt_pqueue);
 
--- include/cstl_queue_private.h
+++ include/cstl_queue_private.h
@@ -56,80 +56,23 @@ typedef struct _tagpriority_queue
 
 /** exported function prototype section **/
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: _create_queue
- *        Parameters: in) t_typesize: size_t
- *                          the element type size.
- *                    in) s_typename: const char*
- *                          the element type name.
- *           Returns: queue_t
- *                          new queue.
- *       Description: create the new queue.
- *
- * ----------------------------------------------------------------------------
+ * Create the new queue.
  */
 extern queue_t _create_queue(size_t t_typesize, const char* s_typename);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: _queue_push
- *        Parameters: in) pt_queue: queue_t*
- *                          the queue.
- *                    in) ...
- *                          the element.
- *           Returns: void
- *       Description: append a copy of element at the top.
- *
- * ----------------------------------------------------------------------------
+ * Append a copy of element at the top.
  */
 extern void _queue_push(queue_t* pt_queue, ...);
-
-/*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: _queue_push_varg
- *        Parameters: in) pt_queue: queue_t*
- *                          the queue.
- *                    in) val_elemlist: va_list
- *                          the element list.
- *           Returns: void
- *       Description: append a copy of element at the top.
- *
- * ----------------------------------------------------------------------------
- */
 extern void _queue_push_varg(queue_t* pt_queue, va_list val_elemlist);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: _create_priority_queue
- *        Parameters: in) t_typesize: size_t
- *                          the element type size.
- *                    in) s_typename: const char*
- *                          the element type name.
- *           Returns: priority_queue_t
- *                          new priority queue.
- *       Description: create the new priority queue.
- *
- * ----------------------------------------------------------------------------
+ * Create the new priority queue.
  */
-extern priority_queue_t _create_priority_queue(
-    size_t t_typesize, const char* s_typename);
+extern priority_queue_t _create_priority_queue(size_t t_typesize, const char* s_typename);
 
 /*
- * ----------------------------------------------------------------------------
- *
- *      Functionname: _priority_queue_push
- *        Parameters: in) pt_pqueue: priority_queue_t*
- *                          the priority queue.
- *                    in) ...
- *                          the element.
- *           Returns: void
- *       Description: append a copy of element at the top.
- *
- * ----------------------------------------------------------------------------
+ * Append a copy of element at the top.
  */
 extern void _priority_queue_push(priority_queue_t* pt_pqueue, ...);
 
