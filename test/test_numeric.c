3f574d1ee23013e45a66e8f3ab65d86d8d70c57d
activesys <activesys.wb@gmail.com>
2010-03-11 13:56:07 +0000

    test numeric algorithm.

--- test/test_numeric.c
+++ test/test_numeric.c
@@ -25,6 +25,7 @@
 #include "cvector.h"
 #include "clist.h"
 #include "cnumeric.h"
+#include "calgorithm.h"
 #include "test_numeric.h"
 
 /** local constant declaration and local macro section **/
@@ -32,6 +33,8 @@
 /** local data type declaration and local struct, union, enum section **/
 
 /** local function prototype section **/
+static void _print_int(const void* cpv_input, void* pv_output);
+static void _print_char(const void* cpv_input, void* pv_output);
 
 /** exported global variable definition section **/
 
@@ -44,50 +47,32 @@ void test_numeric(void)
     {
         vector_t* pt_vec = create_vector(int);
         list_t* pt_list = create_list(char);
-        size_t t_index = 0;
-        iterator_t t_iter;
         if(pt_vec == NULL || pt_list == NULL)
         {
             return;
         }
         vector_init_n(pt_vec, 30);
-        for(t_index = 0; t_index < vector_size(pt_vec); ++t_index)
-        {
-            printf("%d, ", *(int*)vector_at(pt_vec, t_index));
-        }
+        algo_for_each(vector_begin(pt_vec), vector_end(pt_vec), _print_int);
         printf("\n");
         algo_iota(vector_begin(pt_vec), vector_end(pt_vec), -6);
-        for(t_index = 0; t_index < vector_size(pt_vec); ++t_index)
-        {
-            printf("%d, ", *(int*)vector_at(pt_vec, t_index));
-        }
+        algo_for_each(vector_begin(pt_vec), vector_end(pt_vec), _print_int);
         printf("\n");
-        algo_iota(iterator_next_n(vector_begin(pt_vec), 5), iterator_next_n(vector_begin(pt_vec), 15), 100);
-        for(t_index = 0; t_index < vector_size(pt_vec); ++t_index)
-        {
-            printf("%d, ", *(int*)vector_at(pt_vec, t_index));
-        }
+        algo_iota(iterator_next_n(vector_begin(pt_vec), 5),
+            iterator_next_n(vector_begin(pt_vec), 15), 100);
+        algo_for_each(vector_begin(pt_vec), vector_end(pt_vec), _print_int);
         printf("\n");
 
         vector_destroy(pt_vec);
 
         list_init_n(pt_list, 20);
-        for(t_iter = list_begin(pt_list); !iterator_equal(t_iter, list_end(pt_list)); t_iter = iterator_next(t_iter))
-        {
-            printf("%c, ", *(char*)iterator_get_pointer(t_iter));
-        }
+        algo_for_each(list_begin(pt_list), list_end(pt_list), _print_char);
         printf("\n");
         algo_iota(list_begin(pt_list), list_end(pt_list), 'a');
-        for(t_iter = list_begin(pt_list); !iterator_equal(t_iter, list_end(pt_list)); t_iter = iterator_next(t_iter))
-        {
-            printf("%c, ", *(char*)iterator_get_pointer(t_iter));
-        }
+        algo_for_each(list_begin(pt_list), list_end(pt_list), _print_char);
         printf("\n");
-        algo_iota(iterator_advance(list_begin(pt_list), 3), iterator_advance(list_begin(pt_list), 15), 'A');
-        for(t_iter = list_begin(pt_list); !iterator_equal(t_iter, list_end(pt_list)); t_iter = iterator_next(t_iter))
-        {
-            printf("%c, ", *(char*)iterator_get_pointer(t_iter));
-        }
+        algo_iota(iterator_advance(list_begin(pt_list), 3),
+            iterator_advance(list_begin(pt_list), 15), 'A');
+        algo_for_each(list_begin(pt_list), list_end(pt_list), _print_char);
         printf("\n");
 
         list_destroy(pt_list);
@@ -97,7 +82,6 @@ void test_numeric(void)
         list_t* pt_list = create_list(int);
         size_t t_index = 0;
         int n_result = 0;
-        iterator_t t_iter;
         if(pt_list == NULL)
         {
             return;
@@ -107,10 +91,7 @@ void test_numeric(void)
         {
             list_push_back(pt_list, t_index + 1);
         }
-        for(t_iter = list_begin(pt_list); !iterator_equal(t_iter, list_end(pt_list)); t_iter = iterator_next(t_iter))
-        {
-            printf("%d, ", *(int*)iterator_get_pointer(t_iter));
-        }
+        algo_for_each(list_begin(pt_list), list_end(pt_list), _print_int);
         printf("\n");
         algo_accumulate(list_begin(pt_list), list_end(pt_list), 0, &n_result);
         printf("sum : %d\n", n_result);
@@ -120,6 +101,29 @@ void test_numeric(void)
         list_destroy(pt_list);
     }
     /*_algo_accumulate_if        */
+    {
+        list_t* pt_list = create_list(int);
+        size_t t_index = 0;
+        int n_result = 0;
+        if(pt_list == NULL)
+        {
+            return;
+        }
+        list_init(pt_list);
+        for(t_index = 0; t_index < 10; ++t_index)
+        {
+            list_push_back(pt_list, t_index + 1);
+        }
+        algo_for_each(list_begin(pt_list), list_end(pt_list), _print_int);
+        printf("\n");
+        algo_accumulate_if(list_begin(pt_list), list_end(pt_list), 1,
+            fun_multiplies_int, &n_result);
+        printf("product : %d\n", n_result);
+        algo_accumulate_if(list_begin(pt_list), list_end(pt_list), 0,
+            fun_multiplies_int, &n_result);
+        printf("produc : %d\n", n_result);
+        list_destroy(pt_list);
+    }
     /*_algo_inner_product        */
     /*_algo_inner_product_if     */
     /*algo_power                 */
@@ -131,6 +135,17 @@ void test_numeric(void)
 }
 
 /** local function implementation section **/
+static void _print_int(const void* cpv_input, void* pv_output)
+{
+    pv_output = NULL;
+    printf("%d, ", *(int*)cpv_input);
+}
+
+static void _print_char(const void* cpv_input, void* pv_output)
+{
+    pv_output = NULL;
+    printf("%c, ", *(char*)cpv_input);
+}
 
 /** eof **/
 
