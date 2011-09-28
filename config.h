150544f8ce665f46f733d80bb13b308f407115e9
activesys <activesys.wb@gmail.com>
2011-09-28 16:10:24 +0000

    update libcstl revision.

--- config.h
+++ config.h
@@ -73,7 +73,7 @@
 #define PACKAGE_NAME "libcstl"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libcstl 2.0.1"
+#define PACKAGE_STRING "libcstl 2.0.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libcstl"
@@ -82,13 +82,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.1"
+#define PACKAGE_VERSION "2.0.2"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.0.1"
+#define VERSION "2.0.2"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
